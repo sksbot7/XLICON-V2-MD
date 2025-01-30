72259e08f9964405cd4918ad90e6afe64604d85f
sksbot7 <104474096+sksbot7@users.noreply.github.com>
2025-01-30 10:45:52 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,15 +7,13 @@ import axios from 'axios'
 
 // Global variables
 global.owner = [
-  ['918360234087', 'Ｗ Ｈ Ｉ Ｔ Ｅ ４ ４ ４  ? ?', true], 
-  ['923184070915', '𝙎𝘼𝙇𝙈𝘼𝙉 𝘼𝙃𝙈𝘼𝘿 -(𝘼𝙝𝙢𝙢𝙞)', true], 
-  ['233533763772', 'Abraham Dwamena2.0', true],
-  ['233268374753', 'Abraham Dwamena3.0', true]
+  ['919888280858', '𝑆𝑌𝛥𝛭', true], 
+  ['5493884632781', 'SKS OFC ', true]
 ] 
 
-global.mods = ['917888755628','923184070915', '233268374753', '923184474176', '233268374753'] 
-global.prems = ['923184070915', '94789481495', '918360234087', '233268374753']
-global.allowed = ['923184070915', '233268374753', '918360234087', '233268374753']
+global.mods = ['919888280858'] 
+global.prems = ['919888280858,'5493884632781']
+global.allowed = ['919888280858,'5493884632781']
 global.keysZens = ['c2459db922', '37CC845916', '6fb0eff124']
 global.keysxxx = global.keysZens[Math.floor(global.keysZens.length * Math.random())]
 global.keysxteammm = ['29d4b59a4aa687ca', '5LTV57azwaid7dXfz5fzJu', 'cb15ed422c71a2fb', '5bd33b276d41d6b4', 'HIRO', 'kurrxd09', 'ebb6251cc00f9c63']
@@ -49,13 +47,13 @@ global.APIKeys = {
 }
 
 // Sticker WM
-global.botname = '𝘟𝘭𝘪𝘤𝘰𝘯 𝘝𝘰𝘭 2'
+global.botname = 'ꪶ٭𝑺𝜥𝑺 𝐵𝜣𝑇٭ꫂ ᵒᶠᶜ'
 global.premium = 'true'
-global.packname = '𝙏𝙚𝙖𝙢 𝙓𝙇𝙄𝘾𝙊𝙉' 
-global.author = '@𝘮𝘥𝘹𝘭𝘪𝘤𝘰𝘯𝘷2' 
-global.menuvid = 'https://files.catbox.moe/yu3j54.mp4'
-global.igfg = '▢✓ Follow Our channel\nhttps://whatsapp.com/channel/0029VaMGgVL3WHTNkhzHik3c\n' 
-global.dygp = 'https://chat.whatsapp.com/BfH0KLkICn2BjmGFMRcGMW'
+global.packname = 'ꪶ٭𝑺𝜥𝑺 𝐵𝜣𝑇٭ꫂ ᵒᶠᶜ' 
+global.author = '@ig| _sks_bot_' 
+global.menuvid = 'https://cdn.ironman.my.id/q/NDdju.jpg'
+global.igfg = '▢✓ Follow Our channel\nhttps://chat.whatsapp.com/J8mhSOivTA16HOn4KVbadF\n' 
+global.dygp = 'https://chat.whatsapp.com/J8mhSOivTA16HOn4KVbadF'
 global.fgsc = 'https://github.com/salmanytofficial/XLICON-V2-MD' 
 global.fgyt = 'https://youtube.com/@s4salmanyt'
 global.fgpyp = 'https://youtube.com/@s4salmanyt'
